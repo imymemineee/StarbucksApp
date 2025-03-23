5e606f3b755d814532a5b3d215afe0d0a99f4f62
imymemineee <chominyoung2468@gmail.com>
2025-03-24 04:44:03 +0900

    Initial Commit

---
+++ For_Starbucks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  For_Starbucks
+//
+//  Created by 민영조 on 3/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ For_Starbucks/For_StarbucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  For_StarbucksApp.swift
+//  For_Starbucks
+//
+//  Created by 민영조 on 3/24/25.
+//
+
+import SwiftUI
+
+@main
+struct For_StarbucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
